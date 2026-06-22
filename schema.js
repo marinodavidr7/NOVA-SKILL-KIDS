d444fd880fed012480393e376e13bff7e7b1ef36
marinodavidr7 <marinodavidr7@gmail.com>
2026-06-22 12:03:09 -0400

    chore: Clean up temporary migration and debugging scripts

--- schema.js
+++
@@ -1,7 +0,0 @@
-const db = require('better-sqlite3')('estancia.db');
-const tables = db.prepare("SELECT name, sql FROM sqlite_master WHERE type='table'").all();
-tables.forEach(t => {
-  console.log(t.name);
-  console.log(t.sql);
-  console.log('---');
-});
