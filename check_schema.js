dafa73a38c495a54f0bb855a52820587118c38d7
marinodavidr7 <marinodavidr7@gmail.com>
2026-06-22 12:18:57 -0400

    chore: Clean up remaining debugging scripts

--- check_schema.js
+++
@@ -1,9 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('estancia.db');
-
-try {
-  const schema = db.prepare("PRAGMA table_info(medical_records)").all();
-  console.log("Schema:", schema);
-} catch (e) {
-  console.error(e);
-}
